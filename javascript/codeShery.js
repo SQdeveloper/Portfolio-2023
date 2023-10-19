6ff94ff9757fe0f5e51f8ef856381afa0c6442b1
Jefferson <jefferson.silvaquinto@gmail.com>
2023-10-18 23:13:40 -0500

    Revert "optimized videos"
    This reverts commit d45f1385e255047dfbba90a02d7c776ff31017d1.

--- javascript/codeShery.js
+++ javascript/codeShery.js
@@ -1,9 +1,10 @@
 Shery.makeMagnet(".magnet");
 Shery.mouseFollower();
-<<<<<<< HEAD
-=======
 
->>>>>>> d45f138 (optimized videos)
+Shery.hoverWithMediaCircle(".hover-target" /* Element to target.*/, {
+    // images: ["../assets/img/code.jpg", "../assets/img/Apple-page-1.png", "../assets/img/Pariona.png"] /*OR*/,
+    videos: ["../assets/videos/hackerMask.mp4", "../assets/videos/girlMachine.mp4", "../assets/videos/keyboard.mp4"],
+  });
 // Shery.textAnimate(".text-target");
 
 // Shery.imageEffect(".image", {
