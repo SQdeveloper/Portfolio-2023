98c4f649d057915980a3f02e40796ec49436f402
Jefferson <jefferson.silvaquinto@gmail.com>
2023-10-19 01:17:23 -0500

    adding effect text stroke and circle on hover text showing video

--- javascript/codeShery.js
+++ javascript/codeShery.js
@@ -1,9 +1,7 @@
 Shery.makeMagnet(".magnet");
 Shery.mouseFollower();
 
-// Shery.textAnimate(".text-target");
-
-// Shery.imageEffect(".image", {
-//     style: 1 /*OR 5 for different variant */,
-//     debug: true,
-//   });
+Shery.hoverWithMediaCircle(".hover-target" /* Element to target.*/, {
+    // images: ["../assets/img/code.jpg", "../assets/img/Apple-page-1.png", "../assets/img/Pariona.png"] /*OR*/,
+    videos: ["../assets/videos/hackerMask.mp4", "../assets/videos/girlMachine.mp4", "../assets/videos/keyboard.mp4"],
+});
