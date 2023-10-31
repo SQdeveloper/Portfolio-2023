f603fb2751cd1e9858d249db70cf3cdbbda075fe
Jefferson <jefferson.silvaquinto@gmail.com>
2023-10-30 22:22:19 -0500

    remove dependencies

--- javascript/codeShery.js
+++ javascript/codeShery.js
@@ -2,5 +2,13 @@ Shery.makeMagnet(".magnet");
 Shery.mouseFollower();
 
 Shery.hoverWithMediaCircle(".hover-target" /* Element to target.*/, {
-    videos: ["../assets/videos/hackerMask.mp4", "../assets/videos/girlMachine.mp4", "../assets/videos/keyboard.mp4"],
+    videos: ["../assets/videos/hackerMask.mp4", "../assets/videos/girlMachine.mp4", "../assets/videos/keyboard.mp4"],   
 });
+
+Shery.imageMasker(".target-view-proyect" /* Element to target.*/, {
+    //Parameters are optional.
+    mouseFollower: true,
+    text: "View Proyect",
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,12 @@
+import "./configGsap.js"
+import "./carousel.js"
+import "./menu.js";
+import "./changeBackgroundColor.js"
+import "./animationIntoTitle.js"
+import "./codeShery.js"
+import "./certificationsAnimation.js"
+import "./titleCertifications.js"
+
 const parrafo = document.querySelector(".home_textCircle");
 
 parrafo.innerHTML = parrafo.textContent.split("").map((char, i)=>
